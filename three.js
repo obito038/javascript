489ff4845e5ac6feea7761c5491ee0f0c55b0fae
Prasun kr Mondal <89583693+obito038@users.noreply.github.com>
2024-01-10 23:42:40 +0530

    Add files via upload

---
+++ three.js
@@ -0,0 +1,54 @@
+// for of
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+for (const num of arr) {
+    // console.log(num);
+}
+
+const greetings = "Hello World"
+for (const greet of greetings) {
+    // console.log(`Each char is ${greet}`);
+    
+}
+
+
+
+// Maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('PAK', "Pakistan")
+map.set('IN', "India")
+
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    // console.log(key, ':-', value);
+}
+
+
+
+// const myObject = {
+//     'game1' : 'Helo',
+//     'game2' : 'PES'
+
+// }
+
+// for (const [key, value] of object) {
+//     console.log(key, ':-', value);
+// }
+const myObject = {
+    game1 : 'Helo',
+    game2 : 'PES'
+
+}
+
+for (const [key, value] of myObject) {
+    console.log(key, ':-', value);
+}
+
