e9e210cc2691614e765ef9db1533f70d9305fbac
Prasun kr Mondal <89583693+obito038@users.noreply.github.com>
2024-01-11 23:48:14 +0530

    Add files via upload

---
+++ six.js
@@ -0,0 +1,29 @@
+// FOREACH
+// const coding = ["js", "py", "cpp", "java"]
+// const values = coding.forEach( (item)=> {
+//     console.log(item);
+//     return item
+// } )
+
+// console.log(values);
+
+
+////////////////// example///////////////
+
+// Way1 - FILTER 
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// const newNums = myNums.filter( (num)=> {
+//     return  num > 4
+// } )
+
+
+// Way2 - ForEach
+// const newNums = []
+// myNums.forEach( (num)=> {
+//     if (num > 4) {
+//         newNums.push(num)
+//     }
+// } )
+
+// console.log(newNums);
