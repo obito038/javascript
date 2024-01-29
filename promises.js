f83ed4d18cc154a6ddf97a1f6f337dfdd889db4d
Prasun kr Mondal <89583693+obito038@users.noreply.github.com>
2024-01-29 23:58:44 +0530

    Update promises.js

--- promises.js
+++ promises.js
@@ -1,12 +1,70 @@
-onst promiseOne = new Promise(function(resolve, reject){
+// const promiseOne = new Promise(function(resolve, reject){
     //Do an async task
-    // DB calls, cryptography, network
+    //DB calls, cryptography, network call
+//     setTimeout(function(){
+//         console.log('Async thask is cmplt');
+//         resolve()
+//     }, 1000)
+// })
+
+// promiseOne.then(function(){
+//     console.log("Promise consumed");
+// })
+
+
+
+
+
+//way2
+
+// new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         console.log("Async task 2");
+//         resolve()
+//     }, 1000)
+// }).then(function(){
+//     console.log("Async to resloved");
+// })
+
+
+
+
+
+
+//Way3
+
+const promiseThree = new Promise(function(resolve, reject){
     setTimeout(function(){
-        console.log('Async task is compelete');
-        resolve()
+        resolve({username: "pm", email: "pm@gmai.com"})
     }, 1000)
 })
 
-promiseOne.then(function(){
-    console.log("Promise consumed");
+promiseThree.then(function(user){
+    console.log(user);
 })
+
+
+
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true
+        if(!error){
+            resolve({username: "pm", password: "123"})
+        } else{
+            reject('ERROR: Something went wrong')
+        }
+    }, 1000)
+})
+// promiseFour.then().catch()
+promiseFour
+.then((user) => {
+    console.log(user);
+    return user.username
+})
+.then((username) => {
+    console.log(username);
+})
+.catch(function(error){
+    console.log(error);
+})
+.finally()
