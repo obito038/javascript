5f2a511f277e67d5cef36e418dc115580786b3bd
Prasun kr Mondal <89583693+obito038@users.noreply.github.com>
2024-01-30 22:12:19 +0530

    Update promises.js

--- promises.js
+++ promises.js
@@ -67,4 +67,54 @@ promiseFour
 .catch(function(error){
     console.log(error);
 })
-.finally()
+.finally(() => {
+    console.log("The promise is either resolved or rejected");
+})
+
+
+const promiseFive = new Promise(function (resolve, reject) {
+    setTimeout(function(){
+        let error = true
+        if(!error){
+            resolve({username: "javacsript", password: "123"})
+        } else {
+            reject('ERROR: Js went wrong')
+        }
+    }, 1000)
+})
+
+async function consumePromiseFive(){
+    try {
+        const response = await promiseFive
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
+consumePromiseFive()
+
+
+// async function getAllUsers(){
+//     try {
+//         const response = await fetch('https://api.github.com/users/hiteshchoudhary')
+//         // console.log(response);
+//         const data = await response.json()
+//         console.log(data);
+//     } catch (error) {
+//         console.log("E: ", error);
+//     }
+// }
+
+// getAllUsers()
+
+
+
+
+fetch('https://api.github.com/users/hiteshchoudhary')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})                      /* chainable*/
+.catch((error) => console.log(error))
